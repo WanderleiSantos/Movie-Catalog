409ef27253a986d96b269ad0a94ebb9344f4fba8
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2016-03-03 21:17:46 -0300

    Estrutura de pacotes

---
+++ app/src/main/java/com/wanderlei/moviecatalog/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wanderlei.moviecatalog.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wanderlei.moviecatalog.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
